63f87a26d5c681153e338e34f7d813139b0ff9ce
IlayX5G <150474954+IlayX5G@users.noreply.github.com>
2023-11-30 20:21:36 +0200

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,9 @@
+pip install requests
+pip install auto_py_to_exe
+pip install pyinstaller
+pip install pycryptodome
+pip install cryptography
+pip install Crypto.Cipher
+pip install pycryptodome
+pip install fernet
+python main.py
